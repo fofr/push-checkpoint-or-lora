f93957d5b2be82eb51d02700e5dd945335cf6fb0
Paul <fofr@users.noreply.github.com>
2024-06-04 11:39:41 +0000

    Remove enums. Use correct filename for output

--- train.py
+++ train.py
@@ -1,42 +1,9 @@
 import tarfile
 import json
+import io
 from cog import BaseModel, Input, Path
 
 api_json_file = "workflow_api.json"
-
-SAMPLERS = [
-    "euler",
-    "euler_ancestral",
-    "heun",
-    "heunpp2",
-    "dpm_2",
-    "dpm_2_ancestral",
-    "lms",
-    "dpm_fast",
-    "dpm_adaptive",
-    "dpmpp_2s_ancestral",
-    "dpmpp_sde",
-    "dpmpp_sde_gpu",
-    "dpmpp_2m",
-    "dpmpp_2m_sde",
-    "dpmpp_2m_sde_gpu",
-    "dpmpp_3m_sde",
-    "dpmpp_3m_sde_gpu",
-    "ddpm",
-    "lcm",
-    "ddim",
-    "uni_pc",
-    "uni_pc_bh2",
-]
-
-SCHEDULERS = [
-    "normal",
-    "karras",
-    "exponential",
-    "sgm_uniform",
-    "simple",
-    "ddim_uniform",
-]
 
 
 class TrainingOutput(BaseModel):
@@ -44,8 +11,8 @@ class TrainingOutput(BaseModel):
 
 
 def train(
-    weights_string: str = Input(
-        description="A string containing the weights to download. "
+    checkpoint_filename: str = Input(
+        description="The filename of the checkpoint to use. "
         "This can be a URL to a tarball containing the weights or a path to the weights file.",
     ),
     steps: int = Input(
@@ -56,12 +23,10 @@ def train(
     ),
     sampler: str = Input(
         description="The sampler to use during inference",
-        choices=SAMPLERS,
         default="dpmpp_2m_sde_gpu",
     ),
     scheduler: str = Input(
         description="The scheduler to use during inference",
-        choices=SCHEDULERS,
         default="karras",
     ),
 ) -> TrainingOutput:
@@ -75,14 +40,13 @@ def train(
     sampler_node["scheduler"] = scheduler
 
     checkpoint_loader = workflow["4"]["inputs"]
-    checkpoint_loader["ckpt_name"] = weights_string
-
-    # Write the modified data to a new JSON file
-    with open("new_workflow_api.json", "w") as new_file:
-        json.dump(workflow, new_file, indent=4)
+    checkpoint_loader["ckpt_name"] = checkpoint_filename
 
     # Create a tar file for the new workflow data
     with tarfile.open("workflow.tar", "w") as tar:
-        tar.add("new_workflow_api.json", arcname="new_workflow_data.json")
+        tarinfo = tarfile.TarInfo(name="workflow_api.json")
+        workflow_data = json.dumps(workflow, indent=2).encode("utf-8")
+        tarinfo.size = len(workflow_data)
+        tar.addfile(tarinfo, io.BytesIO(workflow_data))
 
     return TrainingOutput(weights=Path("workflow.tar"))
